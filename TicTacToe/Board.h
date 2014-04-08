295080bb15f428e71d40514a2e7643ca2a347b2d
Cameron Beyer <cam.beyer@gmail.com>
2014-04-08 19:18:15 -0400

    Added code

---
+++ TicTacToe/Board.h
@@ -0,0 +1,42 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <vector>
+
+using namespace std;
+
+struct thePoint
+{
+	int X;
+	int Y;
+};
+
+struct pointPair {
+	thePoint one;
+	thePoint two;
+	vector<thePoint> colinears;
+};
+
+class Board
+{
+public:
+   Board(int size = 3);
+   void blankBoard();
+   void displayBoard();
+   void buildPathways();
+   void buildAllTwoPointPairs();
+   void removeDuplicatePairs();
+   bool areOnSameLine(thePoint, thePoint, thePoint);
+   void findColinears();
+   void removeBlankPaths();
+   void displayColinears();
+   bool makeMove(int, int);
+   bool checkIfWin(int);
+   int bestMove(int);
+private:
+   int boardSize;
+   vector< vector<int> > spaces;
+   vector< pointPair > pairs;
+};
+
+#endif
---
+++ TicTacToe/TicTacToe.cpp
@@ -0,0 +1,93 @@
+#include <iomanip>
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include "Board.h"
+
+using namespace std;
+
+void displayWelcome()
+{
+	cout << "Welcome!!" << endl << endl;
+	system("pause");
+	system("cls");
+}
+
+Board chooseBoard()
+{
+	int size = 0;
+	string sizeInput = "";
+	while (size <= 0)
+	{
+		cout << "Please enter the desired size of the board: ";
+		cin >> sizeInput;
+		size = atoi(sizeInput.c_str());
+	}
+	Board theBoard(size);
+	return theBoard;
+}
+
+bool checkWin(int player, Board theBoard)
+{
+	if (theBoard.checkIfWin(player))
+	{
+		cout << "Player " << player << " wins!!!" << endl;
+		system("pause");
+		return true;
+	}
+	else
+	{
+		cout << "Player " << player << " has not won." << endl;
+		return false;
+	}
+}
+
+bool playGame(Board theBoard)
+{
+	theBoard.displayColinears(); //debug line; shows all possible win paths
+	system("cls");
+
+	/* ABOUT TO SIMULTATE PLAYER 1 CHOOSING POSITIONS 1, 2, AND 3! TO SEE OUTPUT, SET BOARD SIZE TO 3 WHEN PROMPTED */
+	int player = 1;
+	while (!checkWin(player, theBoard))
+	{
+		theBoard.displayBoard();
+		string move = "";
+		int theMove = -1;
+		while (theMove < 0)
+		{
+			cout << "Player " << player << ", please make a move: ";
+			cin >> move;
+			theMove = atoi(move.c_str());
+		}
+		if (theMove == 0)
+			theBoard.makeMove(player, theBoard.bestMove(player));
+		else
+			theBoard.makeMove(player, theMove);
+		theBoard.displayBoard();
+		if (checkWin(player, theBoard))
+			break;
+		if (player == 1)
+			player = 2;
+		else
+			player = 1;
+	}
+	cout << "Play again?" << endl;
+	return false;
+}
+
+int main()
+{
+	displayWelcome();
+	bool play = true;
+	Board theBoard;
+	while (play)
+	{
+		theBoard = chooseBoard();
+		system("cls");
+		play = playGame(theBoard);
+		system("cls");
+	}
+	cout << "Thanks for playing TicTacToe!" << endl << endl;
+	system("pause");
+}
