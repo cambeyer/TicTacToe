ab4f033fbc5bac7eeeeb6263cf8c549ea2ae1e0b
dperkinsjr <dperkins@ltu.edu>
2014-04-29 20:13:48 -0400

    Added welcome screen along with comments in the .cpp.

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -6,18 +6,34 @@
 
 using namespace std;
 
+//Simply output the welcome screen to the user that shall describe the game and instructions.
 void displayWelcome()
 {
-	cout << "Welcome!!" << endl << endl;
+	cout << "TicTacToe Version 1.0" << endl 
+		<< "Created By: Cameron Beyer, Dominic Bucchare, and Dean Perkins" << endl 
+		<< endl
+		<< "The rules remain just like any normal Tic Tac Toe game and are as follows:" << endl
+		<< "The goal of Tic Tac Toe is to be the first player to get three in a row on" << endl 
+		<< "a 3x3 grid, or four in a row in a 4x4 grid, etc." << endl 
+		<< endl
+		<< "In the following screen you shall define the size of the board." << endl
+		<< "From there the board will be generated for you and you can play :D." << endl 
+		<< endl
+		<< "All each player must do is simply enter the location on the board (as the " << endl
+		<< "number shown), to place their piece. A winner will be announced once someone" << endl
+		<< "successfully gets their pieces in a row, a tie shall be announced if it occurs." << endl
+		<< endl
+		<< "Enjoy the game :D" << endl;
 	system("pause");
-	system("cls");
+	system("cls"); //Clears the screen at the end
 }
 
+//Gets the determining size from the user for the Board to be initialized.
 Board chooseBoard()
 {
 	int size = 0;
 	string sizeInput = "";
-	while (size < 2 || size > 10)
+	while (size < 2 || size > 10) //Maximum of ten was created do to the console window set size
 	{
 		cout << "Please enter the desired size of the board (min 2, max 10): ";
 		cin >> sizeInput;
@@ -28,6 +44,7 @@ Board chooseBoard()
 	return theBoard;
 }
 
+//Runs the "checkIfWin" function of the board and outputs the final result if someone wins or players tie.
 bool checkWin(int player, Board theBoard)
 {
 	if (theBoard.checkIfWin(player))
@@ -35,12 +52,14 @@ bool checkWin(int player, Board theBoard)
 		cout << "Player " << player << " wins!!!" << endl;
 		return true;
 	}
-	else
+	else if (theBoard.movesRemaining() == 0)
 	{
-		return false;
+		cout << "Cats Game (Players have tied).";
+		return true; 
 	}
 }
 
+//Function to play the game.
 bool playGame(Board theBoard)
 {
 	//theBoard.displayColinears(); //debug line; shows all possible win paths
